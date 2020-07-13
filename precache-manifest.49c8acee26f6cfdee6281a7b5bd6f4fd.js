98f3ee1c2eaf989ac7e3b5656609d797fe631122
Ary <aryascencio@gmail.com>
2020-07-12 23:19:50 -0700

    Updates

---
+++ precache-manifest.49c8acee26f6cfdee6281a7b5bd6f4fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f04ef51fda36cc8a0771fc440d7c7fe",
+    "url": "/typoes-react/index.html"
+  },
+  {
+    "revision": "300e02eaded198638ffd",
+    "url": "/typoes-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5eda74c04adb38a043de",
+    "url": "/typoes-react/static/js/2.a7bc55f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typoes-react/static/js/2.a7bc55f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300e02eaded198638ffd",
+    "url": "/typoes-react/static/js/main.648c4a96.chunk.js"
+  },
+  {
+    "revision": "82ca781aa442e06bf436",
+    "url": "/typoes-react/static/js/runtime-main.7991a592.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/typoes-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
